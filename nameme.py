a98b2308ee4cbd0c298ccb7b1794c92d7c09e29d
ravinu <ravinu.gcom@gmail.com>
2022-08-13 17:09:30 +0530

    def create-file

--- nameme.py
+++ nameme.py
@@ -1,6 +1,13 @@
+from asyncore import file_dispatcher
 import os
+def create-file:
+    f = open(name.txt, 'x')
 print("A simple program to input and output your name")
 fname = input("Input First Name: ")
 lname = input("Input Last Name: ")
 print(fname,lname)
 choice = input("Save as name in file? [Y/n]")
+if choice == "Y":
+ print("yes")
+else:
+    print("no")
