f4298b0ece01c3007d2245e20f2b7301cbffa42a
ravinu <ravinu.gcom@gmail.com>
2022-08-15 06:25:33 +0530

    add mysql support

--- msql.py
+++ msql.py
@@ -3,7 +3,11 @@ print("A simple program to input and output your name")
 fname = input("Input First Name: ")
 lname = input("Input Last Name: ")
 print(fname,lname)
-choice = input("Save as name in file? [Y/n]")
+choice = input("Save as name in Database? [Y/n]")
 if choice == "Y":
+    name = mysql.connector.connect(
+        firstN=fname,
+        lastN=lname
+    )
 else:
-    print("no")
+    print("Not saving to Database")
