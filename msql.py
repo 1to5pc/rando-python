fe42477b2260c1f2db3f7ee7b3146ef558d2e432
ravinu <ravinu.gcom@gmail.com>
2022-08-15 06:04:25 +0530

    remove redundant code

--- msql.py
+++ msql.py
@@ -1,15 +1,9 @@
 import mysql.connector
-f = "name.txt"
-#f = open(f, "x")
-#def create(file):
- #   f = open(f.txt, 'x')
 print("A simple program to input and output your name")
 fname = input("Input First Name: ")
 lname = input("Input Last Name: ")
 print(fname,lname)
 choice = input("Save as name in file? [Y/n]")
 if choice == "Y":
-# f = open(f, "w")
- f.write(fname)
 else:
     print("no")
