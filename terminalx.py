3bc034b8ece6ae42107a497f9f5646a8084077bf
ravinu <ravinu.gcom@gmail.com>
2022-08-25 19:36:28 +0530

    add animations

--- terminalx.py
+++ terminalx.py
@@ -1,5 +1,14 @@
 import time
 import os
+def progress(percent=0, width=20):
+    # The number of hashes to show is based on the percent passed in. The
+    # number of blanks is whatever space is left after.
+    hashes = width * percent // 100
+    blanks = width - hashes
+
+    print('\r[', hashes*'#', blanks*' ', ']', f' {percent:.0f}%', sep='',
+        end='', flush=True)
+
 print("Welcome to Terminalx")
 print("This is a 'terminal emulator' in python")
 cmd = input("Enter your command: ")
@@ -11,8 +20,11 @@ if cmd == "py":
     print("[OK] Python3 Opened")
 elif cmd == "exit":
     print("exiting terminalx")
-    time.sleep(0.2)
-    print("E-X-I-T | T-E-R-M-I-N-A-L_X")
     time.sleep(0.08)
+    time.sleep(0.2)
+    for i in range(101):
+     progress(i)
+     time.sleep(0.01)
+    print()
     print("[OK] Quit 'Terminalx'")
     time.sleep(2)
