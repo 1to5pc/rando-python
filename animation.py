1cb87ef75dec09874bf3bec8b52f2200fe9fe290
ravinu <ravinu.gcom@gmail.com>
2022-08-25 19:39:07 +0530

    reset changes

--- animation.py
+++ animation.py
@@ -2,7 +2,7 @@
 #!/usr/bin/env python
 from time import sleep
 
-def progress(percent=0, width=30):
+def progress(percent=0, width=20):
     # The number of hashes to show is based on the percent passed in. The
     # number of blanks is whatever space is left after.
     hashes = width * percent // 100
@@ -11,10 +11,10 @@ def progress(percent=0, width=30):
     print('\r[', hashes*'#', blanks*' ', ']', f' {percent:.0f}%', sep='',
         end='', flush=True)
 
-print('This will take a moment')
+#print('This will take a moment')
 for i in range(101):
     progress(i)
-    sleep(0.1)
+    sleep(0.01)
 
 # Newline so command prompt isn't on the same line
 print()
