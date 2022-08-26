7134ba746b0103617c1506199232c97b95087c99
ravinu <ravinu.gcom@gmail.com>
2022-08-26 06:12:11 +0530

    fix dependency issue

--- terminalx.py
+++ terminalx.py
@@ -1,13 +1,6 @@
 import time
 import os
-def progress(percent=0, width=20):
-    # The number of hashes to show is based on the percent passed in. The
-    # number of blanks is whatever space is left after.
-    hashes = width * percent // 100
-    blanks = width - hashes
-
-    print('\r[', hashes*'#', blanks*' ', ']', f' {percent:.0f}%', sep='',
-        end='', flush=True)
+import animation
 
 print("Welcome to Terminalx")
 print("This is a 'terminal emulator' in python")
@@ -23,7 +16,7 @@ elif cmd == "exit":
     time.sleep(0.08)
     time.sleep(0.2)
     for i in range(101):
-     progress(i)
+     animation.progress(i)
      time.sleep(0.01)
     print()
     print("[OK] Quit 'Terminalx'")
