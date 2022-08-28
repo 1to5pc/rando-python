d0e9af678382ddbc57ab22aa7b10be830e83d4ef
ravinu <ravinu.gcom@gmail.com>
2022-08-28 18:21:32 +0530

    def "boot"

--- boot.py
+++ boot.py
@@ -1,8 +1,9 @@
 #boot Sequence
 import imp
 import time
-print("Initiating bootloader")
-time.sleep(0.3)
-contbt = input("Boot into pyOS 20.69? (Y)es/(N)o")
-if contbt == "Y":
-    print("Initiating boot sequence")
+def boot():
+ print("Initiating bootloader")
+ time.sleep(0.3)
+ contbt = input("Boot into pyOS 20.69? (Y)es/(N)o")
+ if contbt == "Y":
+     print("Initiating boot sequence")
