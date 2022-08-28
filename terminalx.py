62af5da5c17ca2009fec37e6e1f5eb15b7ab34df
ravinu <ravinu.gcom@gmail.com>
2022-08-28 18:30:32 +0530

    add break & fix indentation

--- terminalx.py
+++ terminalx.py
@@ -10,13 +10,13 @@ print("Welcome to Terminalx")
 print("This is a 'terminal emulator' in python")
 while Lp <= 1:
  cmd = input("Enter your command: ")
-if cmd == "py":
+ if cmd == "py":
       print("Entering python.")
       time.sleep(0.16)
       print("P-Y-T-H-O-N")
       time.sleep(0.3)
       print("[OK] Python3 Opened")
-elif cmd == "exit":
+ elif cmd == "exit":
       print("exiting terminalx")
       time.sleep(0.08)
       time.sleep(0.2)
@@ -26,3 +26,4 @@ elif cmd == "exit":
       print()
       print("[OK] Quit 'Terminalx'")
       time.sleep(0.069)
+      break
